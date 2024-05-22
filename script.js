c548b8011b7edad86d152baaf61c256391c4c75c
Mully <brandon@jokerbusinesssolutions.com>
2024-05-21 19:23:40 -0700

    Created array of selections for computer to randomly get.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+let array = [
+    'rock',
+    'paper',
+    'scissors'
+];
+
+let humanScore;
+
+function getComputerChoice(){
+    
+    let value = Math.floor(Math.random() * array.length);
+    return value;
+}
+// console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Rock paper or scissors? ");
+}
