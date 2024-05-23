5ec73e7054a7ab822cae1aca6666b57a9a8a147f
Mully <brandon@jokerbusinesssolutions.com>
2024-05-23 16:03:48 -0700

    Added code from work

--- script.js
+++ script.js
@@ -1,80 +1,92 @@
-let arr = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
+playRound();
 
-function random(mn, mx){
-    return Math.random() * (mx - mn) + mn;
+let arr = ["Rock", "Paper", "Scissors"];
+
+function random(mn, mx) {
+  return Math.random() * (mx - mn) + mn;
 }
 
+function getComputerChoice() {
+  // This has been tested. It pulls the string name instead of index value.
 
+  let computerSelection = arr[Math.floor(random(1, 4)) - 1];
 
-
-function getComputerChoice(){
-
-    //TODO: Get it to pull string types instead of index values
-    let computerSelection = arr[Math.floor(randon(1, 4)) - 1];
-    return computerSelection;
-
+  return computerSelection;
 }
-// console.log(getComputerChoice());
+
+// This function has been tested and works
 
 function getHumanChoice() {
+  let humanSelection = prompt("Rock paper or scissors? ").toLowerCase();
 
-    let humanSelection = prompt("Rock paper or scissors? ").toLowerCase();
-    return humanSelection;
-
+  return humanSelection;
 }
 
-function playRound(humanSelection, computerSelection){
+function playRound(humanSelection, computerSelection) {
+  let humanScore = 0;
 
-    let humanScore = 0;
-    let computerScore = 0;
+  let computerScore = 0;
 
-    let playGame = prompt("Would you like to play the game? Y/N");
+  let playGame = prompt("Would you like to play the game? Y/N");
 
+  getHumanChoice();
 
-    while (playGame == 'Y' || playGame == 'y'){
+  getComputerChoice();
 
-        getHumanChoice();
-        getComputerChoice();
+  while (playGame == "Y" || playGame == "y") {
+    if (
+      (humanSelection == "rock" && computerSelection == "scissors") ||
+      (humanSelection == "paper" && computerSelection == "rock") ||
+      (humanSelection == "scissors" && computerSelection == "paper")
+    ) {
+      // Display the human won
 
-        if  (humanSelection == 'rock' && computerSelection == 'scissors'
-            || humanSelection == 'paper' && computerSelection == 'rock'
-            || humanSelection == 'scissors' && computerSelection == 'paper') { 
-            
-                // Display the human won
-                alert("You won this round!");
+      console.log("You won this round! The score is being incremented.");
 
-                // Increment humanScore
-                humanScore++;
+      // Increment humanScore
 
-                // Play again
-                playRound(); 
+      humanScore++;
 
-               
-                
-        }
-        else if (computerSelection == 'rock' && humanSelection == 'scissors'
-            || computerSelection == 'paper' && humanSelection == 'rock'
-            || computerSelection == 'scissors' && humanSelection == 'paper') {
-                
-                // Display the computer wom
-                // Increment computerScore
-                // Play again or exit
-            }
-        else {
-            alert("Final score was Player: " +  humanSelection + " and computer: " + computerSelection);
-            return;
-        }
+      // Play again
 
-        
-    
+      playRound();
+    } else if (
+      (computerSelection == "rock" && humanSelection == "scissors") ||
+      (computerSelection == "paper" && humanSelection == "rock") ||
+      (computerSelection == "scissors" && humanSelection == "paper")
+    ) {
+      // Display the computer won
+
+      console.log(
+        "The computer won this round! The score is being incremented."
+      );
+
+      // Increment computerScore
+
+      computerScore++;
+
+      // Play again or exit
+
+      playRound();
+    } else if (
+      (humanSelection == "rock" && computerSelection == "rock") ||
+      (humanSelection == "paper" && computerSelection == "paper") ||
+      (humanSelection == "scissors" && computerSelection == "scissors")
+    ) {
+      console.log("This round was a TIE.");
+
+      // Play again or exit
+
+      playRound();
+    } else {
+      alert(
+        "Final score was Player: " +
+          humanSelection +
+          " and computer: " +
+          computerSelection
+      );
+
+      return;
     }
+  }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
